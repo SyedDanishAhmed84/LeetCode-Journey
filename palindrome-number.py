dc9f1d38aabadac8dffb8c916fcfd15f7fc58922
Syed Danish Ahmed <daniahmed758@gmail.com>
2025-07-13 14:40:45 +0500

    palindrome-number.py

--- palindrome-number.py
+++ palindrome-number.py
@@ -5,11 +5,12 @@ class Solution(object):
      if x==0:
         return True
      if x%10==0:
-        return True
+        return False
      originalX=x
      num_reversed=0
      while x>0:
         last_digit=x%10
-        num_reversed=num_revsersed*10+last_digit
-        x=x//10  
+        num_reversed=num_reversed*10+last_digit
+        x=x//10
+     return originalX==num_reversed   
         
